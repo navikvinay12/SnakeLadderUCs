eb375a7a552b033d81c920d508e6c3ca19d95332
Vinay <navik.vinay12@gmail.com>
2022-11-14 10:25:34 +0530

    added snake ladder NoPlay

--- Program.cs
+++ Program.cs
@@ -12,12 +12,30 @@
             int diceOutput = random.Next(1, 7);
             return diceOutput;
         }
+        public static void Case()
+        {
+            Random random = new Random();
+            int diceCheck = random.Next(1, 4);
+            switch (diceCheck)
+            {
+                case 1:
+                    Console.WriteLine("Ladder");
+                    break;
+                case 2:
+                    Console.WriteLine("Snake");
+                    break;
+                default:
+                    Console.WriteLine("No Play");
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
             WelcomeMsg();
             int position = 0;
+            Console.WriteLine("Initial Position :" + position);
             Console.WriteLine("Dice Output :" + DiceThrown());
+            Case();
         }
     }
 }
-__
