05c5b8f056eb0fbb4283bc41166a255e843eab7a
Vinay <navik.vinay12@gmail.com>
2022-11-14 10:19:35 +0530

    Added welcome msg and set position 0

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SnakeLadderUCs
+{
+    internal class Program
+    {
+        public static void WelcomeMsg()
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game");
+        }
+        static void Main(string[] args)
+        {
+            WelcomeMsg();
+            int position = 0;
+            Console.WriteLine($"Initial starting position of Player :{position}");
+        }
+    }
+}
