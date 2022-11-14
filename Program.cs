291c1059daec20c852c7077507bff2330a39703d
Vinay <navik.vinay12@gmail.com>
2022-11-14 10:22:49 +0530

    added dice to roll 1-6

--- Program.cs
+++ Program.cs
@@ -6,11 +6,18 @@
         {
             Console.WriteLine("Welcome to Snake and Ladder Game");
         }
+        public static int DiceThrown()
+        {
+            Random random = new Random();
+            int diceOutput = random.Next(1, 7);
+            return diceOutput;
+        }
         static void Main(string[] args)
         {
             WelcomeMsg();
             int position = 0;
-            Console.WriteLine($"Initial starting position of Player :{position}");
+            Console.WriteLine("Dice Output :" + DiceThrown());
         }
     }
 }
+__
